8f42c41fc9b8f59afeb3022b8537f72a0138e309
artfultoucher <f.boehme@cs.ucc.ie>
2018-09-25 05:10:14 +0100

    Email suffix checked at registration

--- app/Http/Requests/Frontend/Auth/RegisterRequest.php
+++ app/Http/Requests/Frontend/Auth/RegisterRequest.php
@@ -31,7 +31,7 @@ class RegisterRequest extends FormRequest
         return [
             'first_name'           => 'required|string|max:191',
             'last_name'            => 'required|string|max:191',
-            'email'                => ['required', 'string', 'email', 'max:191', Rule::unique('users')],
+            'email'                => ['required', 'string', 'email', 'max:191', Rule::unique('users'), 'regex:/(.*)@ucc\.ie/'],
             'password'             => 'required|string|min:6|confirmed',
             'g-recaptcha-response' => ['required_if:captcha_status,true', new CaptchaRule()],
         ];
